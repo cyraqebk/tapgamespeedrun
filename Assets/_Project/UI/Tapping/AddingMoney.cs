2cda578cfb6fef53ca45a91b07a628aa5bdb062a
PetrochenkoPetr <pet.13petr@gmail.com>
2024-10-03 23:20:26 +0300

    удалил лишний скрипт
    Я решил, что четвёртый скрипт здесь не нужен и почти не выполняет никакой функции, а также я поменял названия полей, классов, методов, чтобы они были более менее по смыслу...

--- Assets/_Project/UI/Tapping/AddingMoney.cs
+++ Assets/_Project/UI/Tapping/AddingMoney.cs
@@ -7,25 +7,21 @@ namespace UI.Tappings
     public class AddingMoney : MonoBehaviour
     {
         [SerializeField] private TMP_Text moneyText;
-        [SerializeField] private AddMoney addMoney;
+        [SerializeField] private SoftCurrency softCurrency;
 
         private void OnEnable()
         {
-            // Подписываемся на событие изменения валюты через MoneyField
-            addMoney.MoneyField.Changed += OnMoneyChanged;
-            // Обновляем текст на экране
-            moneyText.text = addMoney.Money.ToString();
+            softCurrency.CurrencyField.Changed += OnMoneyChanged;
+            moneyText.text = softCurrency.CurrentAmount.ToString();
         }
 
         private void OnDisable()
         {
-            // Отписываемся от события изменения валюты
-            addMoney.MoneyField.Changed -= OnMoneyChanged;
+            softCurrency.CurrencyField.Changed -= OnMoneyChanged;
         }
 
         private void OnMoneyChanged(int oldValue, int newValue)
         {
-            // Обновляем текст при изменении валюты
             moneyText.text = newValue.ToString();
         }
     }
--- Assets/_Project/UI/Tapping/ButtonClick.cs
+++ Assets/_Project/UI/Tapping/ButtonClick.cs
@@ -6,13 +6,12 @@ namespace UI.Tappings
 {
     public class ButtonClickHandler : MonoBehaviour, IPointerClickHandler
     {
-        [SerializeField] private AddMoney addMoney;
-        [SerializeField] private int buttonValue;
+        [SerializeField] private SoftCurrency softCurrency;
+        [SerializeField] private int currencyIncrement;
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            // При клике изменяем значение через приватный сеттер внутри AddMoney
-            addMoney.Money += buttonValue;
+            softCurrency.CurrentAmount += currencyIncrement;
         }
     }
 }
